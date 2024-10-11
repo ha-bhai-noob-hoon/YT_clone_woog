8acff7e0a1f805dc73000f484c537ffa89ec6701
Abhijit Pandey <abhijitpandey524@gmail.com>
2024-10-11 15:34:04 +0530

    setup components navbar and sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 function Home() {
   return (
-    <div>Home page</div>
+    <div className='max-h-screen overflow-hidden'>
+        <div style={{height:"7.5vh"}}>
+        <Navbar/>
+        </div>
+        <div className='flex' style={{height:"92.5vh"}}>
+            <Sidebar />
+        </div>
+    </div>
+    
   )
 }
 
